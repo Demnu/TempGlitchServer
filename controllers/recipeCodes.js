9421deba554b06a8cfac80c0f80db9a366e1606b
Demnu <harrison.joe.collins@gmail.com>
2022-10-08 18:55:26 +1100

    More work on recipe code functionality

--- controllers/recipeCodes.js
+++ controllers/recipeCodes.js
@@ -1,11 +1,21 @@
 const RecipeCode = require("../models/RecipeCode")
 const createRecipeCode = async (req, res)=>{
+  let duplicate = false;
+
     console.log(req.body);
     result = await RecipeCode.create(req.body).catch(function (error){
       console.log(error)
       duplicate = true;
     })
-    res.status(200).send(result);
+    if (duplicate){
+      console.log("is a duplicate")
+      res.status(400).send("Error! The code and/or blend name has already been saved");
+    }
+    else{
+      console.log("is not a duplicate")
+      res.status(201).send(result);
+
+    }
 }
 
 const getRecipeCodes = async(req,res) =>{
@@ -14,7 +24,7 @@ const getRecipeCodes = async(req,res) =>{
   recipeCodesMongo.forEach((recipeCode)=>{
     recipeCodes.push({id: recipeCode.id,code:recipeCode.code, blendName: recipeCode.blendName})
   })
-  res.status(200).send(recipeCodes);
+  res.status(201).send(recipeCodes);
 
 }
 module.exports = {createRecipeCode, getRecipeCodes}
